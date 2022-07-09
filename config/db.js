9d30ae5438e35d6475531ce14b4cab30b38d10f1
Leston <lestonair6574@gmail.com>
2022-07-09 18:15:08 +0530

    Error handling in server

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI,{
         useNewUrlParser: true,
         // useCreateIndex: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
         // useFindAndModify: true
     })
 
