8a31e83919faf4559b60f3dd623e461825f0d913
Leston <lestonair6574@gmail.com>
2022-08-05 20:32:27 +0530

    frontend routing setup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 const App = () => {
   return (
-    <div className='App'>
-      React App
+   <Router>
+     <div className='App'>
+     <Routes>
+
+     <Route path='/login' element={<></>}> </Route>
+     <Route path='/register' element={<></>}> </Route>
+     <Route path='/forgotpassword' element={<></>}> </Route>
+     <Route path='/passwordreset/:resetToken' element={<></>}> </Route>
+
+     </Routes>
     </div>
+   </Router>
   )
 }
 
