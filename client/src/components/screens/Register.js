37734ac571722185ee703d496ebde05564fb4002
Leston <lestonair6574@gmail.com>
2022-08-06 18:03:08 +0530

    some state creation

--- client/src/components/screens/Register.js
+++ client/src/components/screens/Register.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./register.css"
+import axios from "axios"
+import { Link } from 'react-router-dom'
 
 const Register = () => {
+    const [username,setUsername]=useState("")
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    const [confirmPassword,setConfirmPassword]=useState("")
+
+  function registerHandler(){
+    
+  }
+
   return (
     <div className="one-c">
     <div className="two-c">
@@ -17,7 +28,11 @@ const Register = () => {
                         <p className="conner">Name :</p>
                     </div>
                     <div className="flexer-two newer">
-                        <input type="text" className="name-input input-control" placeholder="Enter your name"/>
+                        <input type="text" className="name-input input-control" placeholder="Enter your name" 
+                        value={username}
+                        onChange={(e)=>{
+                            setUsername(e.target.value)
+                        }}/>
                     </div>
                 </div>
                
@@ -28,7 +43,11 @@ const Register = () => {
                         <p className="conner">Email :</p>
                     </div>
                     <div className="flexer-two">
-                        <input type="text" className="email-input input-control" placeholder="Enter your email"/>
+                        <input type="text" className="email-input input-control" placeholder="Enter your email"
+                        value={email}
+                        onChange={(e)=>{
+                            setEmail(e.target.value)
+                        }}/>
                     </div>
                 </div>
                 
@@ -39,7 +58,12 @@ const Register = () => {
                         <p className="conner">Password :</p>
                     </div>
                     <div className="flexer-two">
-                        <input type="text" className="password-input input-control" placeholder="Enter your password "/>
+                        <input type="text" className="password-input input-control" placeholder="Enter your password "
+                        value={password}
+                        onChange={(e)=>{
+                            setPassword(e.target.value)
+                        }}
+                        />
                     </div>
                 </div>
                
@@ -50,16 +74,22 @@ const Register = () => {
                         <p className="conner">Confirm Password :</p>
                     </div>
                     <div className="flexer-two">
-                        <input type="text" className="password-input input-control" placeholder="Enter your password "/>
+                        <input type="text" className="password-input input-control" placeholder="Enter your password "
+                        value={confirmPassword}
+                        onChange={(e)=>{
+                            setConfirmPassword(e.target.value)
+                        }}
+                        />
                     </div>
                 </div>
                
             </div>
             <div className="submit-section">
-                <button className="submit-button">
-                    Submit
+                <button type="submit" className="submit-button" onClick={registerHandler}>
+                    Register
                 </button>
             </div>
+            <span>Already have an account ? <Link to="/login">Login</Link></span>
         </div>
     </div>
 </div>
