6563543c2cb3269cacbb54d255cac25d46e3915c
Leston <lestonair6574@gmail.com>
2022-08-06 18:12:28 +0530

    setting up axios and some error handling

--- client/src/components/screens/Register.js
+++ client/src/components/screens/Register.js
@@ -3,14 +3,44 @@ import "./register.css"
 import axios from "axios"
 import { Link } from 'react-router-dom'
 
-const Register = () => {
+const Register = ({history}) => {
     const [username,setUsername]=useState("")
     const [email,setEmail]=useState("")
     const [password,setPassword]=useState("")
     const [confirmPassword,setConfirmPassword]=useState("")
+    const [error,setError]=useState("")
 
-  function registerHandler(){
-    
+  function registerHandler(e){
+    e.preventDefault()
+
+    const config={
+        header:{
+            "Content-Type":"application/json"
+        }
+    }
+
+    if (password!==confirmPassword){
+        setPassword("")
+        setConfirmPassword("")
+        setTimeout(()=>{
+            setError("")
+        },5000)
+        return setError("Passowrds do not match")
+    }
+
+    try {
+        const {data}=await axios.post("/api/auth/register",{username,email,password},config)
+
+        localStorage.setItem("authToken",data.token)
+
+        history.push("/")
+    } catch (error) {
+        setError(error.response.data.error)
+        setTimeout(()=>{
+        setError("")
+        },5000)
+    }
+
   }
 
   return (
